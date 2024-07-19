51383d778a03bc2713c50a71d77238647cd6e245
GalvisDev <galvisdeveloper@gmail.com>
2024-07-18 22:17:17 -0500

    Capa de rest server con controlador y rutas para el todo

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,24 +1,35 @@
 
 
-import express from 'express';
+import express, { Router } from 'express';
 
 interface Options {
     PORT: number;
+    routes: Router;
 }
 
 export class Server {
     private app = express();
     private readonly port: number;
+    private readonly routes: Router;
 
     constructor(options: Options) {
         this.port = options.PORT;
+        this.routes = options.routes;
     }
 
     async start() {
+
+        // * Middleware
+        this.app.use(express.json()); // - raw
+        this.app.use(express.urlencoded({ extended: true })); // - x-www-form-urlencoded
+
         // * Public folder
         this.app.use(express.static('public'));
 
-        // * Middleware
+        // * Routes
+        this.app.use(this.routes);
+
+        // * SPA
         this.app.get('*', (req, res) => {
             res.sendFile('index.html', { root: 'public' });
         });
@@ -28,11 +39,4 @@ export class Server {
         });
     }
 
-    get(path: string, handler: express.RequestHandler) {
-        this.app.get(path, handler);
-    }
-
-    post(path: string, handler: express.RequestHandler) {
-        this.app.post(path, handler);
-    }
 }
---
+++ src/presentation/todos/controller.ts
@@ -0,0 +1,59 @@
+import { Request, Response } from "express";
+
+
+const todos = [
+    { id: 1, text: 'Buy Milk', completedAt: new Date() },
+    { id: 2, text: 'Buy Eggs', completedAt: new Date() },
+    { id: 3, text: 'Buy Mame', completedAt: new Date() },
+    { id: 4, text: 'Buy Cabeza', completedAt: new Date() },
+]
+
+export class TodoController {
+
+    constructor() { }
+
+    async getTodos(req: Request, res: Response) {
+        res.json(todos);
+    }
+
+    async getTodoById(req: Request, res: Response) {
+        const { id } = req.params;
+        const todo = todos.find(todo => todo.id === +id);
+        if (!todo) {
+            return res.status(404).json({ message: 'Todo not found' });
+        }
+        res.json(todo);
+    }
+
+    async createTodo(req: Request, res: Response) {
+        const { text } = req.body;
+        if (!text) {
+            return res.status(400).json({ message: 'Text is required' });
+        }
+        const newTodo = {
+            id: todos.length + 1,
+            text,
+            completedAt: new Date()
+        }
+        todos.push(newTodo);
+        res.status(201).json(newTodo);
+    }
+
+    async updateTodo(req: Request, res: Response) {
+        const { id } = req.params;
+        if (isNaN(+id)) return res.status(400).json({ message: 'Invalid id' });
+
+        const { text } = req.body;
+        if (!text) return res.status(400).json({ message: 'Text is required' });
+
+        const todo = todos.find(todo => todo.id === +id);
+        if (!todo) {
+            return res.status(404).json({ message: 'Todo not found' });
+        }
+        todo.text = text || todo.text;
+
+        res.json(todo);
+    }
+
+
+}
