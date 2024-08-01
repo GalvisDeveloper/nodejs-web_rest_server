7a4af8b659dd26af08d09c1b6ce464eaaf6de375
GalvisDev <galvisdeveloper@gmail.com>
2024-07-31 21:08:09 -0500

    DataSourceImpl para prisma

--- src/domain/entities/todo/todo.entity.ts
+++ src/domain/entities/todo/todo.entity.ts
@@ -12,5 +12,22 @@ export class TodoEntity {
         return !!this.completedAt;
     }
 
+    public static fromObject = (object: { [key: string]: any }): TodoEntity => {
+        const { id, text, completedAt } = object;
+
+        if (!id) throw new Error('Id is required');
+        if (!text) throw new Error('Text is required');
+
+        let newCompletedAt = null;
+        if (completedAt) {
+            newCompletedAt = new Date(completedAt);
+            if (isNaN(newCompletedAt.getTime())) {
+                throw new Error('Invalid date');
+            }
+        }
+
+        return new TodoEntity(id, text, completedAt);
+    }
+
 }
 
---
+++ src/domain/index.ts
@@ -0,0 +1,6 @@
+
+
+export * from './datasources/todo/todo.datasource';
+export * from './repositories/todo/todo.repository';
+export * from './entities/todo/todo.entity';
+export * from './dtos';
