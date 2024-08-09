bf4b79465c1d1520ba0c32b4846a7f83c497177a
GalvisDev <galvisdeveloper@gmail.com>
2024-08-08 21:59:09 -0500

    Testing unitario get all todos

--- tests/presentation/todos/routes.test.ts
+++ tests/presentation/todos/routes.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import { testServer } from "../../test-server";
+import { prisma } from "../../../src/data/postgres";
 
 
 
@@ -14,13 +15,23 @@ describe('Unit Test Todo Routes', () => {
         await testServer.stop();
     });
 
-    test('should return TODOS list', async () => {
+    const todo1 = { text: 'todo1' };
+    const todo2 = { text: 'todo2' };
+    const todo3 = { text: 'todo3' };
+    const todo4 = { text: 'todo4' };
+    const todo5 = { text: 'todo5' };
 
-        const response = await request(testServer.app)
+    test('should return TODOS list', async () => {
+        await prisma.todo.deleteMany();
+        await prisma.todo.createMany({ data: [todo1, todo2, todo3, todo4, todo5] });
+
+        const { body } = await request(testServer.app)
             .get('/api/todos')
             .expect(200);
 
-        console.log(response.body);
+        expect(body).toBeInstanceOf(Array);
+        expect(body).toHaveLength(5);
+        expect(body[0].text).toBe(todo1.text);
     });
 
 
